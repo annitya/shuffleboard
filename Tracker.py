da4bb131404a7174bd1d547ad623aaae87484baf
Henning Kvinnesland <henning@byte.no>
2016-04-15 15:16:54 +0200

    Refactored out tracker.

---
+++ Tracker.py
@@ -0,0 +1,87 @@
+from collections import deque
+import imutils
+import Puck
+import cv2
+import numpy as np
+
+
+class Tracker:
+    lowerLimit = None
+    upperLimit = None
+    minRadius = None
+    pts = None
+    buffer = 64
+
+    def __init__(self, lower_limit, upper_limit, min_radius):
+        self.lowerLimit = lower_limit
+        self.upperLimit = upper_limit
+        self.minRadius = min_radius
+        self.pts = deque(maxlen=self.buffer)
+
+    def track(self, frame, table):
+        # resize the frame, blur it, and convert it to the HSV
+        # color space
+        frame = imutils.resize(frame, height=400)
+        # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # construct a mask for the color "green", then perform
+        # a series of dilations and erosions to remove any small
+        # blobs left in the mask
+        mask = cv2.inRange(hsv, self.lowerLimit, self.upperLimit)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+        cv2.imshow("Mask", mask)
+
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+        for contour in contours:
+            ((x, y), radius) = cv2.minEnclosingCircle(contour)
+            if radius < self.minRadius:
+                continue
+
+            m = cv2.moments(contour)
+            center = (int(m["m10"] / m["m00"]), int(m["m01"] / m["m00"]))
+            puck = Puck.Puck(center, radius)
+            table.greenPucks.append(puck)
+
+        self.render(contours, frame)
+
+    def render(self, contours, frame):
+        for contour in contours:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            # c = max(greenContours, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(contour)
+            m = cv2.moments(contour)
+            center = (int(m["m10"] / m["m00"]), int(m["m01"] / m["m00"]))
+
+            # only proceed if the radius meets a minimum size
+            if radius > self.minRadius:
+                # draw the circle and centroid on the frame,
+                # then update the list of tracked points
+                cv2.circle(frame, (int(x), int(y)), int(radius),
+                           (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+            # update the points queue
+            self.pts.appendleft(center)
+
+        # loop over the set of tracked points
+        for i in xrange(1, len(self.pts)):
+            # if either of the tracked points are None, ignore
+            # them
+            if self.pts[i - 1] is None or self.pts[i] is None:
+                continue
+
+            # otherwise, compute the thickness of the line and
+            # draw the connecting lines
+            thickness = int(np.sqrt(self.buffer / float(i + 1)) * 2.5)
+            cv2.line(frame, self.pts[i - 1], self.pts[i], (0, 0, 255), thickness)
+
+        # show the frame to our screen
+        frame = cv2.flip(frame, 1)
+        cv2.imshow("Frame", frame)
